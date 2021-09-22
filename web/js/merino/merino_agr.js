9b3a1198145d6e5678dcac3c95b291731fc19fe9
Bladimir Huaraya <bladimir.huaraya@gmail.com>
2021-09-22 16:52:34 -0500

    add display laws

--- web/js/merino/merino_agr.js
+++ web/js/merino/merino_agr.js
@@ -1,4 +1,4 @@
-import { fetchData } from '../js/laws.js';
+import { fetchData } from '../../js/laws.js';
 
 document.addEventListener('DOMContentLoaded', e => {
   fetchData('MMerino_period.json', 'agricultura')
